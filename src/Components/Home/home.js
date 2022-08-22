4008135767cdb37158b2267cfecd2f1c81b8e6c8
GesSpark <kachefali@gmail.com>
2022-08-22 15:07:16 -0400

    Intial commit

---
+++ src/Components/Home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './home.css'
+
+const Home = () => {
+    return (
+    <>
+        <div>
+            <h1>Zahra Foundaion</h1>
+        </div>
+    </>
+    )
+}
+
+export default Home;
